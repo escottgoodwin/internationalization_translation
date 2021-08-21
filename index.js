9fd849b0a010f2a8180b4e91318a013ec1fb4255
Evan Goodwin <evansgoodwin@gmail.com>
2021-08-21 15:48:10 -0700

    moved everything to index

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { generateInternationalizationFile } from './generateTranslations.js'
+import fs from 'fs'
+import gTranslate from '@google-cloud/translate';
+const {Translate} = gTranslate.v2;
 
 // be sure to set the environment variable with the path the service account json file
 // and the service account is authorized to use the google translate api
@@ -9,14 +11,55 @@ import { generateInternationalizationFile } from './generateTranslations.js'
 // with an account authorized for the google translate api, 
 // you don't need to set the environment variable
 
-const targetLang = 'fr' // to language
-const sourceFilePath = 'en.json' // source file
-const destinationFilePath = '../lmifb/src/translations' // location to save generated file
+const targetLang = '' // to language
+const sourceFilePath = '' // source file
+const destinationFilePath = '' // location to save generated file
+const googleProjectId = ''
 
-generateInternationalizationFile(
-    targetLang, 
-    sourceFilePath, 
-    destinationFilePath, 
-)
+const translate = new Translate({googleProjectId});
 
+function importSourceFile(sourceTranslation){
+    return JSON.parse(fs.readFileSync(sourceTranslation).toString());
+}
 
+const translateValue = async (value, targetLang) => {
+    const [translation] = await translate.translate(value, targetLang);
+    return translation;
+}
+
+const recursiveObjectPromiseAll = (jsonSourceObj, targetLang, destinationFilePath) => {
+    const keys = Object.keys(jsonSourceObj);
+    return Promise.all(keys.map(async key => {
+      const value = jsonSourceObj[key];
+      if (typeof value === 'object' && !value.then) {
+        return recursiveObjectPromiseAll(value, targetLang);
+      }
+      return await translateValue(value, targetLang);
+    }))
+      .then(result => zipObject(keys, result))
+      .then(data => toJsonFile(data, targetLang, destinationFilePath));
+  };
+
+const zipObject = (keys = [], values = []) => {
+    return keys.reduce((accumulator, key, index) => {
+      accumulator[key] = values[index]
+      return accumulator
+    }, {})
+  }
+
+function toJsonFile(trans, targetLang, destinationFilePath){
+    const jsonfile = JSON.stringify(trans)
+    const path = `${destinationFilePath}/${targetLang}.json`
+    fs.writeFile(path, jsonfile, 'utf8', () => console.log(`wrote ${path}`));
+}
+
+export async function generateInternationalizationFile(){
+    const jsonSourceObj = importSourceFile(sourceFilePath)
+    recursiveObjectPromiseAll(
+        jsonSourceObj, // js obj imported from filex - must contain object name 'const trans'
+        targetLang, 
+        destinationFilePath// language translating to 
+    ) 
+}
+
+generateInternationalizationFile()
